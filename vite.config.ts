13097cd3b6d2eb714009fbcf541c1f0c8753ade4
musagenius345 <musabadru@gmail.com>
2023-10-30 22:25:48 +0300

    path alias resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from "node:path";
+
+const __filename = new URL(import.meta.url).pathname
+const __dirname = path.dirname(__filename)
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      '$org': normalizePath('src/lib/Organisms'),
+      '$atom': normalizePath('src/lib/Atoms'),
+      '$mol': normalizePath('src/lib/Molecules'),
+    },
+  },
 })
+
+function normalizePath(file: string){
+  return path.join(__dirname, file)
+}
