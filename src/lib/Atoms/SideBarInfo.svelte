78c9f8a0c1dcff3ba935b487c606679bdae66ab4
musagenius345 <musabadru@gmail.com>
2023-11-01 00:41:53 +0300

    Sidebar done

---
+++ src/lib/Atoms/SideBarInfo.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let infoTitle = 'Step'
+  export let infoSubtitle = "Step Info"
+</script>
+<div>
+  <h3>{infoTitle}</h3>
+  <h4>{infoSubtitle}</h4>
+</div>
+
+<style>
+  div{
+    text-transform: uppercase;
+  }
+
+  h3{
+    font-weight: 400;
+    padding:  0;
+    margin: 0;
+    color: var(--cool-gray);
+    font-size: 0.85rem;
+  }
+  h4{
+    padding: 0;
+    margin: 0;
+    color: var(--magnolia);
+    font-size: 1.1rem;
+    font-weight: 500;
+  }
+</style>
--- src/lib/Atoms/SideBarNum.svelte
+++ src/lib/Atoms/SideBarNum.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   export let num = 1
+  export let current
 </script>
 
-<div>
+<div class:current>
   {num}
 </div>
 
@@ -11,9 +12,15 @@
     width: 42px;
     height: 42px;
     border-radius: 50%;
-    border: 2px solid var(--cool-gray);
+    border: 2px solid var(--light-gray);
     text-align: center;
     font-size: 1.2rem;
-    font-weigh: 900;
+    font-weight: 700;
+  }
+
+  .current{
+    background-color: hsl(201 70% 85%);
+    background-color: var(--light-blue);
+    color: var(--marine-blue)
   }
 </style>
