1b401d8076bde2678166898066f6f24e1b9b7f10
musagenius345 <musabadru@gmail.com>
2023-10-31 12:55:11 +0300

    title and subitle atoms

---
+++ src/lib/Atoms/StepSubtitle.svelte
@@ -0,0 +1,11 @@
+<h2>
+  <slot />
+</h2>
+
+<style>
+  h2{
+    color: var(--cool-gray);
+    padding: 0;
+    margin: 0;
+  }
+</style>
---
+++ src/lib/Atoms/StepTitle.svelte
@@ -0,0 +1,11 @@
+<h1>
+  <slot />
+</h1>
+
+<style>
+  h1{
+    color: var(--marine-blue);
+    padding: 0;
+    margin: 0;
+  }
+</style>
