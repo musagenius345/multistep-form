5f113a24474c2c1bd134453e1634e897e3455a78
musagenius345 <musabadru@gmail.com>
2023-10-31 12:55:29 +0300

    heading molecule

---
+++ src/lib/Molecules/Heading.svelte
@@ -0,0 +1,18 @@
+<script>
+  import StepTitle from '$atom/StepTitle.svelte'
+  import StepSubtitle from "$atom/StepSubtitle.svelte";
+
+  export let title = 'Title'
+  export let subtitle = 'This is a subtitle'
+</script>
+<div>
+   <StepTitle>{title}</StepTitle>
+   <StepSubtitle>{subtitle}</StepSubtitle>
+</div>
+
+
+<style>
+div{
+  /* text-align: left; */
+}
+</style>
