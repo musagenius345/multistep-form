6b9bcf9a8aeafc1dbdd9e44d3189985994e7440e
musagenius345 <musabadru@gmail.com>
2023-11-01 19:09:47 +0300

    step 2 last component finished with logic

---
+++ src/lib/Molecules/PeriodToggle.svelte
@@ -0,0 +1,26 @@
+<script>
+
+</script>
+
+<div>
+<label for="switch">
+  <span>Monthly</span>
+  <input type="checkbox" name="switch" on:click role="switch">
+  <span>Yearly</span>
+</label>
+</div>
+
+  <style>
+   div {
+     text-align: center;
+     accent-color: var(--marine-blue);
+     background-color: var(--light-blue);
+     padding: .3em 1.2em;
+   }
+
+   span{
+     fonnt-weight: 500;
+   }
+
+   
+</style>
