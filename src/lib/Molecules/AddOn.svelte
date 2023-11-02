a0b1b9f2286b4c81d76f7c4c591516c20cf73fce
musagenius345 <musabadru@gmail.com>
2023-11-02 23:17:13 +0300

    preliminary addOn

--- src/lib/Molecules/AddOn.svelte
+++ src/lib/Molecules/AddOn.svelte
@@ -1,20 +1,72 @@
 <script>
+  export let isChecked = false
+  export let selected = false
+  export let feature 
+  export let description
+  export let fee
+
+  function selectAddOn(){
+      if(!isChecked){
+      isChecked = true
+      selected = true
+    } else {
+      isChecked = false
+      selected = false
+  }
+  }
   
 </script>
 
-<div>
-  <input type="checkbox">
-  <div>
-    <h3>Online Service</h3>
-    <p>Access to Multiplayer Games</p>
+<button class="cover" class:selected on:click={selectAddOn}>
+  <input class="checkbox" type="checkbox" checked={isChecked}>
+  <div class="text">
+    <h3>{feature}</h3>
+    <p class="desc">{description}</p>
   </div>
-  <div class"price">
-    <p>$10/yr</p>
+  <div >
+    <p class="price">+${fee}/yr</p>
   </div>
-</div>
+</button>
 
-<style lang="scss">
-  div{
-    color: red;
+<style>
+  h3{
+    padding: 0;
+    margin: 0;
   }
+.price{
+    color: var(--purplish-blue)
+  }
+
+  .cover{
+    /* all: unset; */
+    background-color: inherit;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    cursor: pointer;    
+    outline: 0;
+    border: 0;
+    border: 2px solid var(--light-gray);
+  }
+  .checkbox{
+    --primary: var(--purplish-blue);
+  } 
+  .cover:is(:focus, :hover){
+    border: 3px solid var(--marine-blue);
+    background-color: var(--pasteel-blue);
+    outline: 0;
+  }
+ 
+  .selected{
+    border: 3px solid var(--marine-blue);
+    background-color: var(--pasteel-blue);
+
+  }
+
+   p{
+    color: var(--cool-gray);
+  }
+
+
+
 </style>
