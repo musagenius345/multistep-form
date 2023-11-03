76c03953b346d283c8412bd69e26f7a93d9a0877
musagenius345 <musabadru@gmail.com>
2023-11-03 03:37:26 +0300

    toggle logic improved

--- src/lib/Molecules/PeriodToggle.svelte
+++ src/lib/Molecules/PeriodToggle.svelte
@@ -1,25 +1,48 @@
 <script>
-
+  export let yearly
 </script>
 
 <div>
 <label for="switch">
-  <span>Monthly</span>
-  <input type="checkbox" name="switch" on:click role="switch">
-  <span>Yearly</span>
+  <span class:monthly={!yearly}>Monthly</span>
+  <input class="switch" type="checkbox" name="switch" on:click role="switch">
+  <span class:yearly>Yearly</span>
 </label>
 </div>
 
   <style>
    div {
      text-align: center;
-     accent-color: var(--marine-blue);
-     background-color: var(--light-blue);
-     padding: .3em 1.2em;
+     /* accent-color: var(--marine-blue); */
+     background-color: var(--magnolia);
+     /*padding: .3em 1.2em;*/
    }
 
+   label{
+     padding: 1.0rem 0.4rem;
+     display:flex;
+     justify-content: center;
+     align-items: center;
+     gap: 0.3em;
+   }
+
+   .switch{
+     /* accent-color: var(--marine-blue); */
+     /* accent-color: red; */
+     /* --primary: var(--marine-blue); */
+   }
+   
+    .yearly{
+      color: var(--marine-blue);
+    }
+    .monthly{
+      color: var(--marine-blue);
+    }
+
+
    span{
-     fonnt-weight: 500;
+     font-weight: 500;
+     color: var(--cool-gray);
    }
 
    
