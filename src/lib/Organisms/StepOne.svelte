8594806094eb96fb270a6aae663a850a4064d198
musagenius345 <musabadru@gmail.com>
2023-11-05 00:32:42 +0300

    validation neary complete, css styling left

--- src/lib/Organisms/StepOne.svelte
+++ src/lib/Organisms/StepOne.svelte
@@ -1,72 +1,96 @@
+
 <script lang="ts">
-  import { personalInfo } from '$store/personalInfo'
-  import { isEmpty, isEmail, isMobilePhone } from 'validator'
-  let nameWarning = '' 
-  let phoneWarning = ''
-  let emailWarning = ''
-  function validateName(){
-     return $personalInfo.name.isEmpty()
+  import { personalInfo } from '$store/personalInfo';
+  import { isEmpty, isEmail, isMobilePhone } from 'validator';
+  
+  let nameWarning = 'Field required';
+  let phoneWarning = 'Invalid Number';
+  let emailWarning = 'Invalid Email';
+  
+  function validateName() {
+    return isEmpty($personalInfo.name);
   }
-
+  
+  function validatePhone() {
+    return !isMobilePhone($personalInfo.tel);
+  }
+  
+  function validateEmail() {
+    return !isEmail($personalInfo.email);
+  }
+  
   function handleSubmit() {
-
-      // If the form is valid, proceed with form submission
+    if (!validateName() && !validatePhone() && !validateEmail()) {
+      // Form is valid, proceed with form submission
       alert('Form submitted successfully!');
+    } else {
+      // Form is invalid, display validation warnings
+      if (validateName()) nameWarning = 'Name is required';
+      if (validatePhone()) phoneWarning = 'Invalid Phone Number';
+      if (validateEmail()) emailWarning = 'Invalid Email Address';
     }
+  }
 </script>
-  <form on:submit|preventDefault={() => alert(JSON.stringify($personalInfo))}>
-  <!-- <span style="color: black;">{$personalInfo.name}</span> -->
+
+<form on:submit|preventDefault={handleSubmit}>
   <label for="name">
     <div class="flex">
-    <span>Name</span>
+      <span>Name</span>
       {#if validateName()}
-    <span class="warning">{nameWarning}</span>
+        <span class="warning">{nameWarning}</span>
       {/if}
     </div>
-    <input id="name" type="text" name="name" class:warning={validateName} placeholder="e.g Stephen King" required bind:value={$personalInfo.name} />
+    <input id="name" type="text" name="name" class:warning={validateName} placeholder="e.g Stephen King"  bind:value={$personalInfo.name} min="1" on:blur={validateName}/>
   </label>
 
   <label for="email">
     <div class="flex">
-    <span class="warning">Email Address</span>
+      <span>Email Address</span>
       {#if validateEmail()}
-    <span>{emailWarning}</span>
+        <span class="warning">{emailWarning}</span>
       {/if}
     </div>
-     <input id="email" type="tel" name="email" class:warning={!validateEmail} placeholder="e.g stephenking@lorem.com" required bind:value={$personalInfo.email} />
+    <input id="email" type="email" name="email" class:warning={validateEmail} placeholder="e.g stephenking@lorem.com" bind:value={$personalInfo.email} on:blur={validateEmail} />
   </label>
 
   <label for="phoneNumber">
     <div class="flex">
-    <span>Phone Number</span>
+      <span>Phone Number</span>
       {#if validatePhone()}
-    <span class="warning">{phoneWarning}</span>
+        <span class="warning">{phoneWarning}</span>
       {/if}
     </div>
-     <input id="phoneNumber" type="tel" name="phone number" class:warning={!validatePhone} placeholder="e.g +1 234 567 89o" required bind:value={$personalInfo.tel} />
+    <input id="phoneNumber" type="tel" name="phone number" class:warning={validatePhone} placeholder="e.g +1 234 567 890" min="10" bind:value={$personalInfo.tel}  on:blur={validatePhone}/>
   </label>
-<button type="submit" on:click={handleSubmit}>Submit</button>
+  
+  <!-- <button type="submit">Submit</button> -->
 </form>
 
 <style>
-  .flex{
+  .flex {
     display: flex;
     justify-content: space-between;
   }
-  .warning{
-    border: var(--strawberry-red);
-  }
-  span.warning{
+  
+  .warning {
     color: var(--strawberry-red);
+    
+    /* border-color: var(--strawberry-red); */
   }
- label {
-   color: var(--marine-blue);
-   font-weight: 500;
- }
 
- input:not(:placeholder-shown){
-   font-weight: 700;
-   color: var(--marine-blue);
-   
- }
-</style>
+  input:invalid{
+    border-color: var(--cool-gray);
+  }
+  
+  label {
+    color: var(--marine-blue);
+    font-weight: 500;
+    margin-bottom: 0.5rem;
+  }
+  
+  input:not(:placeholder-shown) {
+    font-weight: 700;
+    color: var(--marine-blue);
+  }
+  
+  </style>
