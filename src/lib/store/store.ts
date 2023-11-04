65b8d12afe3fd00fac775f5e4dee47656bb76488
musagenius345 <musabadru@gmail.com>
2023-11-04 12:41:00 +0300

    persisted store

--- src/lib/store/store.ts
+++ src/lib/store/store.ts
@@ -1,7 +1,20 @@
 
 // store.ts
-import { writable } from 'svelte/store';
+// import { writable } from 'svelte/store';
+import { persisted } from 'svelte-persisted-store'
+
+export const currentStep = persisted("currentStep-MSF", 1)
 
 
-export const currentStep = writable(1)
+const intialData = {
+  name: null,
+  email: null,
+	phoneNumber: null,
+  sectedPlan: null,
+  addOns: []
+}
+
+// First param `preferences` is the local storage key.
+// Second param is the initial value.
+export const data = persisted('MultiStepFormData', intialData)
 
