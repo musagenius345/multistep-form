0cfec495cca81edd1ce042dd5894498e49daf5b8
musagenius345 <musabadru@gmail.com>
2023-11-04 23:30:56 +0300

    light validation setup

--- src/lib/Organisms/StepOne.svelte
+++ src/lib/Organisms/StepOne.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
- // import FormInput from '$atom/FormInput.svelte'
   import { personalInfo } from '$store/personalInfo'
+  function handleSubmit() {
 
+      // If the form is valid, proceed with form submission
+      alert('Form submitted successfully!');
+    }
 </script>
-  <form on:submit|preventDefault={() => console.log(form.data)}>
-
+  <form on:submit|preventDefault={() => alert(JSON.stringify($personalInfo))}>
+  <!-- <span style="color: black;">{$personalInfo.name}</span> -->
   <label for="name"><span>Name</span> 
     <input id="name" type="text" name="name" placeholder="e.g Stephen King" required bind:value={$personalInfo.name} />
   </label>
@@ -13,10 +16,10 @@
      <input id="email" type="tel" name="email" placeholder="e.g stephenking@lorem.com" required bind:value={$personalInfo.email} />
   </label>
 
-  <label for="phoneNumber"><span>Phone number</span> 
+  <label for="phoneNumber"><span>Phone Number</span> 
      <input id="phoneNumber" type="tel" name="phone number" placeholder="e.g +1 234 567 89o" required bind:value={$personalInfo.tel} />
   </label>
-
+<button type="submit" on:click={handleSubmit}>Submit</button>
 </form>
 
 <style>
