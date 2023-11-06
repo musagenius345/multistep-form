305ff33da299f3d8612de4543a645c483b1e67b6
musagenius345 <musabadru@gmail.com>
2023-11-06 20:01:40 +0300

    minor styling

--- src/lib/Organisms/StepFour.svelte
+++ src/lib/Organisms/StepFour.svelte
@@ -2,32 +2,48 @@
   import { data, currentStep } from '$store/store'
   import { period } from '$store/optionsStore'
 
-const formatCurrency = amount => new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(amount);
   $: price = $period === 'mo' ? $data.selectedPlan.price : $data.selectedPlan.yearlyPrice 
+  // Calculate total price including selected plan and add-ons
+  // $: totalAddOnsPrice = $data.addOns.reduce((acc, { fee }) => acc + (fee / 10 * ($period === 'mo' ? 1 : 10)), 0);
+  // $: totalPrice = price + totalAddOnsPrice;
+
+  // $: formattedTotalPrice = formatCurrency(totalPrice);
+const formatCurrency = amount => new Intl.NumberFormat('en-US', { style: 'currency', minimumFractionDigits: 0, maximumFractionDigits: 0,currency: 'USD' }).format(amount);
+  $: billing = $period === 'mo' ? 'Monthly' : 'Yearly'
   // $: formattedPrice = formatCurrency(price)
 </script>
-
-<fieldset class="flex fieldset">
- <div class="flex">
+<div class="container">
+<fieldset class="flex fieldset spacing">
+ <div class="flex planDiv">
     <div>
-      <p class="plan">{$data.selectedPlan.heading}</p>
+      <p class="plan">{$data.selectedPlan.heading}({billing})</p>
       <button on:click={() => $currentStep = 2}>Change</button>
     </div>
-    <p>{formatCurrency(price)}</p>
+    <p class="price">+{formatCurrency(price)}/{$period}</p>
  </div>
  <div>
   {#each $data.addOns as {feature, fee } } 
     <div class="flex">    
         <p class="feature">{feature}</p>
-        <p class="fee">{formatCurrency(fee)}</p>
+        <p class="fee">+{formatCurrency($period === 'mo' ? fee / 10 : fee)}/{$period}</p>
     </div>
  {/each}
  </div>
  </fieldset>
-
+  <div class="flex spacing">
+    <p>Total</p>
+    <p>$1000/{$period}</p>
+  </div>
+</div>
 <style>
+  
   p{
     /* margin: 0; */
+  }
+
+  .planDiv{
+    padding-bottom: .4em;
+    border-bottom: 2px solid var(--cool-gray);
   }
   button{
     all: unset;
@@ -55,18 +71,35 @@ const formatCurrency = amount => new Intl.NumberFormat('en-US', { style: 'curren
     background-color: var(--magnolia);
     font-size: 1.1rem;
     border-radius: 12px;
-    margin: 1rem 1.65rem;
-    padding: 1.25rem 1.85rem;
-    flex-direction: column;
+   flex-direction: column;
     gap: 1rem;
+    
+  }
+
+  .spacing{
+    --margin: 0.5rem 0.8rem;
+    --padding: 0.75rem 0.9125rem;
+    margin: var(--margin);
+    padding: var(--padding);
+
+  }
+
+  .container{
+    margin: 0.5rem 0.225rem;
+    padding: 0.75rem 0.9125rem;
+
   }
 
   .feature{
     color: var(--cool-gray);
     font-size: inherit;
   }
-  .fee{
-    color: inherit;
+  .price, .fee{
+    color: var(--marine-blue);
     font-size: inherit;
+  }
+
+  .price{
+    font-weight: 500;
   }
 </style>
